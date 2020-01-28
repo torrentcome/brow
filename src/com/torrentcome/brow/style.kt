3a71b1043942eb13a2df0b804b576f4311801bf8
torrentcome <torrent.come@gmail.com>
2020-01-29 00:09:17 +0100

    add code

--- src/com/torrentcome/brow/style.kt
+++ src/com/torrentcome/brow/style.kt
@@ -1,6 +1,7 @@
 package com.torrentcome.brow
 
 import java.util.*
+import kotlin.collections.HashMap
 
 typealias PropertyMap = HashMap<String, Value?>
 
@@ -13,7 +14,7 @@ enum class Display {
     None
 }
 
-data class StyleNode<Any>(var node: Node, var specifiedValues: PropertyMap, var children: Vector<StyleNode<Any>>) {
+data class StyledNode(var node: Node, var specifiedValues: PropertyMap, var children: Vector<StyledNode>) {
     // Return the specified value of a property if it exists, otherwise `None`.
     fun value(name: String): Value? {
         return specifiedValues[name]
@@ -46,23 +47,50 @@ data class StyleNode<Any>(var node: Node, var specifiedValues: PropertyMap, var 
 
 class Style {
 
+    /// Apply a stylesheet to an entire DOM tree, returning a StyledNode tree.
+    ///
+    /// This finds only the specified values at the moment. Eventually it should be extended to find the
+    /// computed values too, including inherited values.
+    fun styleTree(root: Node, stylesheet: Stylesheet): StyledNode {
+        return StyledNode(
+                node = root,
+                specifiedValues = when (root.nodeType) {
+                    is NodeType.Element -> specifiedValues(elem = (root.nodeType as NodeType.Element).elementData, stylesheet = stylesheet)
+                    is NodeType.Text -> HashMap()
+                    else -> throw(Exception())
+                },
+                children = root.children.map { child -> styleTree(child, stylesheet) } as Vector<StyledNode>)
+    }
+
     // Apply styles to a single element, returning the specified styles.
     // To do: Allow multiple UA/author/user stylesheets, and implement the cascade.
-    fun specifiedValues(elem: ElementData, stylesheet: Stylesheet) : PropertyMap {
+    fun specifiedValues(elem: ElementData, stylesheet: Stylesheet): PropertyMap {
         val values = PropertyMap()
         val rules = matching_rules(elem, stylesheet)
 
         // Go through the rules from lowest to highest specificity.
         // rules.sortBy { it.selectors }
+        rules.sortBy { i -> i!!.rule.declarations.toString() }
         for (rule in rules) {
-            for (declaration in rule.declarations) {
+            for (declaration in rule!!.rule.declarations) {
                 values[declaration.name] = declaration.value
             }
         }
         return values
     }
 
-    private fun matching_rules(elem: ElementData, stylesheet: Stylesheet): Vector<Rule> {
-        return Vector()
+    fun matching_rules(elem: ElementData, stylesheet: Stylesheet): Vector<MatchedRule?> {
+        val filter: List<MatchedRule?> = stylesheet.rules.map { e -> match_rule(elem, e) }
+        return filter as Vector<MatchedRule?>
     }
+
+    private fun match_rule(elem: ElementData, rule: Rule?): MatchedRule? {
+        val find: Selector? = rule?.selectors?.find { selector -> matches(elem, selector) }
+        return find?.let { MatchedRule(it.specificity(), rule) }
+    }
+
+    private fun matches(elem: ElementData, selector: Selector?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
