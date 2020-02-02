8a03ab77c7af8e224dea8376d7154369b006125f
torrentcome <torrent.come@gmail.com>
2020-02-02 01:44:23 +0100

    works !

--- src/com/torrentcome/brow/painting.kt
+++ src/com/torrentcome/brow/painting.kt
@@ -1,15 +1,39 @@
 package com.torrentcome.brow
 
-import sun.jvm.hotspot.opto.Block
-
 data class Canvas(
-        var pixels: ArrayList<Color> = ArrayList(),
+        var pixels: Array<Color>,
         var width: Float,
         var height: Float) {
 
-    fun paint_item(item: DisplayCommand) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun paintItem(item: DisplayCommand) {
+        when (item) {
+            is SolidColor -> {
+                val rect = item.rect
+                val color = item.color
+                // Clip the rectangle to the canvas boundaries.
+                val x0: Int = rect.x.coerceIn(0.0F, width).toInt()
+                val y0: Int = rect.y.coerceIn(0.0F, height).toInt()
+                val x1: Int = (rect.x + rect.width).coerceIn(0.0F, width).toInt()
+                val y1: Int = (rect.y + rect.height).coerceIn(0.0F, height).toInt()
+
+                for (y in y0..y1) {
+                    for (x in x0..x1) {
+                        // TODO: alpha compositing with existing pixel
+                        pixels[y * width.toInt() + x] = color
+                    }
+                }
+            }
+        }
     }
+}
+
+fun new(width: Float, height: Float): Canvas {
+    val white = Color(r = 255, g = 255, b = 255, a = 255)
+    return Canvas(
+            pixels = Array(width.toInt() * height.toInt()) { white },
+            width = width,
+            height = height
+    )
 }
 
 typealias DisplayList = ArrayList<DisplayCommand>
@@ -20,75 +44,88 @@ data class SolidColor(var color: Color, var rect: Rect) : DisplayCommand()
 object Painting {
     /// Paint a tree of LayoutBoxes to an array of pixels.
     fun paint(layout_root: LayoutBox, bounds: Rect): Canvas {
-        val display_list = build_display_list(layout_root)
-        val canvas = Canvas(width = bounds.width, height = bounds.height)
-        for (item in display_list) {
-            canvas.paint_item(item)
+        val displayList = buildDisplayList(layout_root)
+        val canvas = new(width = bounds.width, height = bounds.height)
+        for (item in displayList) {
+            canvas.paintItem(item)
         }
         return canvas
     }
 
-    private fun build_display_list(layoutRoot: LayoutBox): DisplayList {
+    private fun buildDisplayList(layoutRoot: LayoutBox): DisplayList {
         val list = DisplayList()
-        render_layout_box(list, layoutRoot)
+        renderLayoutBox(list, layoutRoot)
         return list
     }
 
-    private fun render_layout_box(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
-        render_background(list, layout_box)
-        render_borders(list, layout_box)
+    private fun renderLayoutBox(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
+        renderBackground(list, layout_box)
+        renderBorders(list, layout_box)
         for (child in layout_box.children) {
-            render_layout_box(list, child)
+            renderLayoutBox(list, child)
         }
     }
 
-    private fun render_background(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
-        get_color(layout_box, "background")?.let {
+    private fun renderBackground(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
+        getColor(layout_box, "background")?.let {
             list.add(SolidColor(color = it, rect = layout_box.dimensions.borderBox()))
         }
     }
 
-    private fun render_borders(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
-        val color = get_color(layout_box, "border-color") ?: return
+    private fun renderBorders(list: java.util.ArrayList<DisplayCommand>, layout_box: LayoutBox) {
+        val color = getColor(layout_box, "border-color") ?: return
 
         val d = layout_box.dimensions
-        val border_box = d.borderBox()
+        val borderBox = d.borderBox()
 
         // Left border
         list.add(SolidColor(color, Rect(
-                x = border_box.x,
-                y = border_box.y,
+                x = borderBox.x,
+                y = borderBox.y,
                 width = d.border.left,
-                height = border_box.height
+                height = borderBox.height
         )))
 
         // Right border
         list.add(SolidColor(color, Rect(
-                x = border_box.x + border_box.width - d.border.right,
-                y = border_box.y,
+                x = borderBox.x + borderBox.width - d.border.right,
+                y = borderBox.y,
                 width = d.border.right,
-                height = border_box.height
+                height = borderBox.height
         )))
 
         // Top border
         list.add(SolidColor(color, Rect(
-                x = border_box.x,
-                y = border_box.y,
-                width = border_box.width,
+                x = borderBox.x,
+                y = borderBox.y,
+                width = borderBox.width,
                 height = d.border.top
         )))
 
         // Bottom border
         list.add(SolidColor(color, Rect(
-                x = border_box.x,
-                y = border_box.y + border_box.height - d.border.bottom,
-                width = border_box.width,
+                x = borderBox.x,
+                y = borderBox.y + borderBox.height - d.border.bottom,
+                width = borderBox.width,
                 height = d.border.bottom
         )))
     }
 
-    private fun get_color(layoutBox: LayoutBox, s: String): Color? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun getColor(layoutBox: LayoutBox, name: String): Color? {
+        return when (layoutBox.box_type) {
+            is BlockNode -> {
+                val value: Value? = (layoutBox.box_type as BlockNode).styledNode.value(name)
+                if (value is ColorValue) value.color
+                else null
+            }
+            is InlineNode -> {
+                val value: Value? = (layoutBox.box_type as InlineNode).styledNode.value(name)
+                if (value is ColorValue) value.color
+                else null
+            }
+            is AnonymousBlock -> null
+            else -> null
+        }
     }
 
 }
--- src/com/torrentcome/brow/test/test.kt
+++ src/com/torrentcome/brow/test/test.kt
@@ -1,7 +1,12 @@
 package com.torrentcome.brow.test
 
 import com.torrentcome.brow.*
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
 import java.util.*
+import javax.imageio.ImageIO
 import kotlin.test.assertEquals
 
 class HelloKotlin {
@@ -17,22 +22,66 @@ class HelloKotlin {
             val parseCss = css()
             assertEquals(stylesheet, parseCss)
 
-            val styleRoot = style()
+            val styleRoot = style2()
 
             // Since we don't have an actual window, hard-code the "viewport" size.
             val viewport = Dimensions()
-            viewport.content.width  = 800.0f
+            viewport.content.width = 800.0f
             viewport.content.height = 600.0f
 
             val layoutRoot = Layout.layoutTree(styleRoot, viewport)
             println("* layoutRoot = ")
             println("$layoutRoot")
 
+            // Since we don't have an actual window, hard-code the "viewport" size.
+            val initialContainingBlock = Dimensions(Rect(x = 0.0f, y = 0.0f, width = 800.0f, height = 600.0f))
+            val canvas = Painting.paint(layoutRoot, initialContainingBlock.content)
+
+            try {
+                // retrieve image
+                val bi = BufferedImage(canvas.width.toInt(), canvas.height.toInt(), BufferedImage.TYPE_INT_RGB)
+                val g: Graphics = bi.graphics
+
+                var row = 0
+                var line = 0
+                canvas.pixels.forEachIndexed { _, color ->
+                    g.color = color.toOfficial()
+                    g.drawRect(row, line, 1, 1)
+                    if (row >= canvas.width) {
+                        line++
+                        row = 0
+                    }
+                    row++
+                }
+
+                val outputfile = File("saved.png")
+                ImageIO.write(bi, "png", outputfile)
+            } catch (e: IOException) {
+                println(e)
+            }
         }
 
+        @Suppress("unused")
         private fun style(): StyledNode {
             val html2 = "<html> <head> <title>Test</title> </head> <div class=\"outer\"> <p class=\"inner\"> Hello, <span id=\"name\">world!</span> </p> <p class=\"inner\" id=\"bye\"> Goodbye! </p> </div> </html>"
             val css2 = "* { display: block; } span { display: inline; } html { width: 600px; padding: 10px; border-width: 1px; margin: auto; background: #ffffff; } head { display: none; } .outer { background: #00ccff; border-color: #666666; border-width: 2px; margin: 50px; padding: 50px; } .inner { border-color: #cc0000; border-width: 4px; height: 100px; margin-bottom: 20px; width: 500px; } .inner#bye { background: #ffff00; } span#name { background: red; color: white; }"
+
+            val parseHtml2 = Html.parse(html2)
+            println("* parseHtml2 = ")
+            println("$parseHtml2")
+            val parseCss2 = Css.parse(css2)
+            println("* parseCss2 = ")
+            println("$parseCss2")
+
+            val styleTree: StyledNode = Style.styleTree(root = parseHtml2, stylesheet = parseCss2)
+            println("* styleTree = ")
+            println("$styleTree")
+            return styleTree
+        }
+
+        private fun style2(): StyledNode {
+            val html2 = "<html> <div class=\"a\"> <div class=\"b\"> <div class=\"c\"> <div class=\"d\"> <div class=\"e\"> <div class=\"f\"> <div class=\"g\"> </div> </div> </div> </div> </div> </div> </div> </html>"
+            val css2 = "* { display: block; padding: 12px; } .a { background: #ff0000; } .b { background: #ffa500; } .c { background: #ffff00; } .d { background: #008000; } .e { background: #0000ff; } .f { background: #4b0082; } .g { background: #800080; }"
 
             val parseHtml2 = Html.parse(html2)
             println("* parseHtml2 = ")
@@ -95,4 +144,8 @@ class HelloKotlin {
             return rootDom
         }
     }
+}
+
+fun Color.toOfficial(): java.awt.Color {
+    return java.awt.Color(this.r.toInt(), this.g.toInt(), this.b.toInt())
 }
